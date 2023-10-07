36e240938bb6a2ed846fb21a59b5e890983d8e48
erik-whiting <erik@erikwhiting.com>
2023-10-07 10:21:02 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name = 'RNAFoldAssess',
+    version = '0.1',
+    description = 'Training repository for RNA folding',
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Intended Audience :: Science/Research',
+        ],
+    python_requires = '>=3.9',
+    install_requires = [
+        'scipy',
+        'biopython'],
+    packages = ['RNAFoldAssess']
+)
+
