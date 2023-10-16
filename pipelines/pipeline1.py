52cd3dace3c4929cf6ab7436e35517f1cfabbeb3
erik-whiting <erik@erikwhiting.com>
2023-10-15 21:36:47 -0500

    Quick pipeline example works for SPOT-RNA

--- pipelines/pipeline1.py
+++ pipelines/pipeline1.py
@@ -1,5 +1,12 @@
-import re
+import re, path, os
 
+
+from RNAFoldAssess.models import DataPoint
+from RNAFoldAssess.models.scorers import DSCI
+from RNAFoldAssess.models.predictors import *
+
+
+# Prepare data
 
 data_path = "../data/development_data"
 
@@ -19,6 +26,16 @@ class Datum:
                     self.readings.append(float(read))
                 except:
                     continue
+
+def datum_to_datapoint(datum):
+    dhash = {
+        "name": datum.name,
+        "sequence": datum.sequence,
+        "data": datum.readings,
+        "reads": 0 # We don't use reads
+    }
+    return DataPoint(dhash)
+
 
 f = open(data_path)
 raw = f.read()
@@ -44,15 +61,62 @@ for block in raw:
     if (name and sequence and readings):
         data_points.append(Datum(name, sequence, readings))
 
-problems = 0
+data_problems = 0
 for dp in data_points:
     # Making sure the lengths and sequences are working
     if len(dp.sequence) != len(dp.readings):
+        data_problems += 1
         print(f"Something wrong with {dp.name}!!!!!")
         print(f"Sequence length: {len(dp.sequence)}, Readings length: {len(dp.readings)}")
-if problems == 0:
-    print("All good")
+
+if data_problems == 0:
+    print("Data all good")
 else:
-    print(f"There were {problems} problems")
+    print(f"There were {data_problems} problems with the data")
+
+
+# Transform to datapoints because I forgot the models expect that
+dps = []
+for dp in data_points:
+    dps.append(datum_to_datapoint(dp))
+
+if len(dps) == len(data_points):
+    print("Successfully transformed datapoints")
+else:
+    print("Something went wrong in transofrmation")
+
+# Prepare models
+
+predictors = []
+predictors.append({
+    "name": "SPOT-RNA",
+    "model": SPOT_RNA(),
+    "path": path.Path("/home/yesselmanlab/ewhiting/SPOT-RNA").abspath()
+})
+
+predictors.append({
+    "name": "Eterna",
+    "model": Eterna(),
+    "path": path.Path("/home/yesselmanlab/ewhiting/EternaFold").abspath()
+})
+
+# Run predictions
+predictions = []
+scores = []
+
+# Run SPOT-RNA predictions
+predictor = predictors[0]
+for dp in dps:
+    input_file_path = dp.to_fasta_file()
+    print(f"INPUT: {input_file_path}")
+    predictor["model"].execute(predictor["path"], input_file_path, remove_file_when_done=True)
+    prediction = predictor['model'].get_ss_prediction()
+    scorer = DSCI(dp, prediction, "SPOT-RNA")
+    scores.append(scorer.metrics)
+
+print("\nPrinting scores\n")
+for s in scores:
+    print(s)
+
 
 
