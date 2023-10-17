ae360ddcade9e25f7ce9cc74c1ed414e7716f166
erik-whiting <erik@erikwhiting.com>
2023-10-16 19:39:55 -0500

    Add file removal to MXFold

--- RNAFoldAssess/models/predictors/mxfold.py
+++ RNAFoldAssess/models/predictors/mxfold.py
@@ -5,10 +5,13 @@ class MXFold:
     def __int__(self):
         self.output = ""
 
-    def execute(self, path, fasta_file):
+    def execute(self, path, fasta_file, remove_file_when_done=True):
         path_to_mx_fold = os.path.abspath(path)
         exec_string = f"{path} {fasta_file}"
         self.output = os.popen(exec_string).read()
+        if remove_file_when_done:
+            print(f"MXFold object is deleting {fasta_file}")
+            os.remove(fasta_file)
 
     def get_ss_prediction(self):
         if self.output == "":
