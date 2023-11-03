b183292de4f61ef0d686aaa2ad5e6b177016a364
erik-whiting <erik@erikwhiting.com>
2023-11-02 23:08:48 -0500

    Fix failing tests in Swan

--- tests/test_rna_structure.py
+++ tests/test_rna_structure.py
@@ -8,14 +8,15 @@ class TestRNAStructure:
     # Testing with C009C
     base_data_path = "/common/yesselmanlab/ewhiting/ss_deeplearning_data/data"
     datum = DataPoint.factory(f'{base_data_path}/C009C.json')[0]
-    input_file_path = datum.to_fasta_file()
     model = RNAStructure()
     model_path = "/util/opt/anaconda/deployed-conda-envs/packages/rnastructure/envs/rnastructure-6.1/bin/Fold"
-    # For HCC Swan, load the rnastructure module
+    # HCC Swan housekeeping:
     os.system("module load rnastructure")
+    os.system("export DATAPATH=/util/opt/anaconda/deployed-conda-envs/packages/rnastructure/envs/rnastructure-6.1/share/rnastructure/data_tables")
 
     def test_prediction(self):
-        self.model.execute(self.model_path, self.input_file_path, "some_output.ct")
+        input_file_path = self.datum.to_fasta_file()
+        self.model.execute(self.model_path, input_file_path, "some_output.ct")
         prediction = self.model.get_ss_prediction()
         scorer = DSCI(self.datum, self.model.get_ss_prediction(), 'RNAStructure', evaluate_immediately=True, DMS=True)
         metrics = scorer.metrics
--- tests/test_rnafold.py
+++ tests/test_rnafold.py
@@ -8,12 +8,12 @@ class TestRNAFold:
     # Testing with C009C
     base_data_path = "/common/yesselmanlab/ewhiting/ss_deeplearning_data/data"
     datum = DataPoint.factory(f'{base_data_path}/C009C.json')[0]
-    input_file_path = datum.to_fasta_file()
     model = RNAFold()
     model_path = path.Path("/home/yesselmanlab/ewhiting/ViennaRNA/bin/RNAfold")
 
     def test_prediction(self):
-        self.model.execute(self.model_path, self.input_file_path)
+        input_file_path = self.datum.to_fasta_file()
+        self.model.execute(self.model_path, input_file_path)
         prediction = self.model.get_ss_prediction()
         scorer = DSCI(self.datum, self.model.get_ss_prediction(), 'RNAFold', evaluate_immediately=True, DMS=True)
         metrics = scorer.metrics
