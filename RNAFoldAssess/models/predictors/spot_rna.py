dc3aaa69da24b8bca9f0090a9c06d5daad39a048
erik-whiting <erik@erikwhiting.com>
2023-11-05 11:46:37 -0600

    Updates to SPOT but need to revisit

--- RNAFoldAssess/models/predictors/spot_rna.py
+++ RNAFoldAssess/models/predictors/spot_rna.py
@@ -13,21 +13,17 @@ class SPOT_RNA:
         if not os.path.exists("./spot_output"):
             os.mkdir("./spot_output")
         spot_exec_string = f"python3 {spot_rna_path}/SPOT-RNA.py  --inputs {fasta_file}  --outputs ./spot_output"
-        print(f"spot_exec_string: {spot_exec_string}")
         # SPOT-RNA generates several files, we need the  .ct file
         # to get the secondary structure in dot-bracket notation
         conda_exec_string = f"conda run -n {self.conda_env_name} {spot_exec_string}"
-        print(f"\nRunning {conda_exec_string}")
         os.system(conda_exec_string)
         ct_file = fasta_file.split('/')[-1].replace('fasta', 'ct')
         ct_file_path = os.path.abspath(f"spot_output/{ct_file}")
         # Now we need to use the Vienna ct2db tool to
         # get the output in the format we want
         ct2db_exec_string = f"{self.ct2db_path_string} {str(ct_file_path)}"
-        print(f"\nRunning {ct2db_exec_string}")
         self.output = os.popen(ct2db_exec_string).read()
         if remove_file_when_done:
-            print(f"SPOT-RNA object is deleting {fasta_file}")
             os.remove(fasta_file)
 
     def get_ss_prediction(self):
