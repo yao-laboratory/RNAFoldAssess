74251d773c438297b28fbc0764f4c630a52cc65c
erik-whiting <erik@erikwhiting.com>
2023-11-05 12:34:46 -0600

    Use GPU for SPOT-RNA

---
+++ pipelines/spot_rna_dms_pipeline/02_2_generate_predictions_gpu.sh
@@ -0,0 +1,13 @@
+BASE_DATA_PATH=/common/yesselmanlab/ewhiting/data/dp_fasta
+DESTINATION_BASE=/common/yesselmanlab/ewhiting/data/spot_rna_ct_files_gpu
+SPOT_RNA_PATH=/home/yesselmanlab/ewhiting/SPOT-RNA/SPOT-RNA.py
+
+for dir in $(ls $BASE_DATA_PATH); do
+    for f in $(ls $BASE_DATA_PATH/$dir); do
+        FASTA_FILE=$BASE_DATA_PATH/$dir/$f
+        EXEC_STR="python3 $SPOT_RNA_PATH --inputs $FASTA_FILE --outputs $DESTINATION_BASE --gpu 0"
+        conda run -n spot $EXEC_STR
+	rm $DESTINATION_BASE/*.prob
+	rm $DESTINATION_BASE/*.bpseq
+    done
+done
