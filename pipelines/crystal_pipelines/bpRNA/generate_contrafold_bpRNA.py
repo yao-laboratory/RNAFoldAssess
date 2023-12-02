fe0dc88b246697781ff7cd2d77b50eae472737e7
erik-whiting <erik@erikwhiting.com>
2023-12-02 14:26:35 -0600

    Add ContraFold and evaluate Crystal1

---
+++ pipelines/crystal_pipelines/bpRNA/generate_contrafold_bpRNA.py
@@ -0,0 +1,19 @@
+import os, datetime
+
+from RNAFoldAssess.models import DataPointFromCrystal
+from RNAFoldAssess.models.predictors import *
+from RNAFoldAssess.utils import *
+
+model_name = "ContraFold"
+model = ContraFold()
+model_path = os.path.abspath("/home/yesselmanlab/ewhiting/EternaFold")
+data_type_name = "bpRNA-1m-90"
+
+print("starting ...")
+generate_bpRNA_evaluations(
+    model=model,
+    model_name=model_name,
+    model_path=model_path,
+    testing=True
+)
+print("done")
