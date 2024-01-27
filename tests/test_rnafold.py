547ff48aa5129337b2053401ee11bfce6a82f2c1
Erik Whiting <erik@erikwhiting.com>
2024-01-26 23:09:04 -0600

    Use different path

--- tests/test_rnafold.py
+++ tests/test_rnafold.py
@@ -9,8 +9,7 @@ class TestRNAFold:
     base_data_path = "./tests/fixtures"
     datum = DataPoint.factory(f'{base_data_path}/C009C.json')[0]
     model = RNAFold()
-    model_path = os.path.abspath("/home/runner/work/RNAFoldAssess/RNAFoldAssess/ViennaRNA-2.6.4/bin/RNAfold")
-
+    model_path = os.path.abspath("/home/runner/work/RNAFoldAssess/RNAFoldAssess/RNAFoldAssess/ViennaRNA-2.6.4/bin/RNAfold")
     def test_prediction(self):
         input_file_path = self.datum.to_fasta_file()
         self.model.execute(self.model_path, input_file_path, remove_file_when_done=True)
