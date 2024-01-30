83fce9028f574f54113d06f0b6d37b2be7b8ef66
Erik Whiting <erik@erikwhiting.com>
2024-01-30 13:26:57 -0600

    Remove reference to data point decorator

--- RNAFoldAssess/models/__init__.py
+++ RNAFoldAssess/models/__init__.py
@@ -1,6 +1,5 @@
 from .data_point import *
 from .eterna_data_point import *
-from .data_point_decorator import *
 from .data_point_from_crystal import *
 from .predictors import *
 from .scorers import *
