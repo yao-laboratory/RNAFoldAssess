b8e86bf50d5c65af8e926a62a3fa2a8d6990a4ab
Erik Whiting <erik@erikwhiting.com>
2025-02-01 11:33:21 -0600

    Use ViennaRNA for RNAFold class (#36)

--- tests/test_rnafold.py
+++ tests/test_rnafold.py
@@ -5,16 +5,25 @@ import pytest
 from RNAFoldAssess.models import RNAFold, DataPoint
 from RNAFoldAssess.models import DSCI
 
-@pytest.mark.skip(reason="Still figuring out how to make this work in CI")
+# @pytest.mark.skip(reason="Still figuring out how to make this work in CI")
 class TestRNAFold:
     # Testing with C009C
     base_data_path = "./tests/fixtures"
     datum = DataPoint.factory(f'{base_data_path}/C009C.json')[0]
     model = RNAFold()
-    model_path = os.path.abspath("/home/runner/work/RNAFoldAssess/RNAFoldAssess/RNAFoldAssess/ViennaRNA-2.6.4/bin/RNAfold")
-    def test_prediction(self):
+    def test_prediction_with_fasta_file(self):
         input_file_path = self.datum.to_fasta_file()
-        self.model.execute(self.model_path, input_file_path, remove_file_when_done=True)
+        self.model.execute(input_file_path, source_type="fasta")
+        prediction = self.model.get_ss_prediction()
+        scorer = DSCI(self.datum, prediction, 'RNAFold', evaluate_immediately=True, DMS=True)
+        metrics = scorer.metrics
+        assert(metrics['accuracy'] > 0.7)
+
+    def test_prediction_with_string_input(self):
+        base_data_path = "./tests/fixtures"
+        datum = DataPoint.factory(f'{base_data_path}/C009C.json')[0]
+        sequence = datum.sequence
+        self.model.execute(sequence)
         prediction = self.model.get_ss_prediction()
         scorer = DSCI(self.datum, prediction, 'RNAFold', evaluate_immediately=True, DMS=True)
         metrics = scorer.metrics
