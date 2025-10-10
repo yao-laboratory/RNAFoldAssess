b33d045e7a6825f9c7b96b52262bd79cce407c5e
erik-whiting <erik@erikwhiting.com>
2025-10-09 23:42:47 -0500

    Adding type annotations

--- RNAFoldAssess/utils/sequence_tools.py
+++ RNAFoldAssess/utils/sequence_tools.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 class SequenceTools:
     @staticmethod
-    def generate_kmers(sequence, k):
+    def generate_kmers(sequence: str, k: int) -> list[str]:
         kmers = []
         for i in range(len(sequence) - k + 1):
             kmers.append(sequence[i:i+k])
@@ -12,19 +12,19 @@ class SequenceTools:
         return kmers
 
     @staticmethod
-    def count_homopolymers(sequence, k, nucleotide):
+    def count_homopolymers(sequence: str, k: int, nucleotide: str) -> int:
         kmers = SequenceTools.generate_kmers(sequence, k)
         kmer_to_find = nucleotide * k
         return kmers.count(kmer_to_find)
 
     @staticmethod
-    def get_gc_content(sequence):
-        g_count = sequence.upper.count("G")
-        c_count = sequence.upper.count("C")
+    def get_gc_content(sequence: str) -> float:
+        g_count = sequence.upper().count("G")
+        c_count = sequence.upper().count("C")
         return (g_count + c_count) / len(sequence)
 
     @staticmethod
-    def get_sequence_entropy(sequence, log_base=4):
+    def get_sequence_entropy(sequence: str, log_base: int = 4) -> float:
         # Count nucleotide frequencies
         counts = Counter(sequence)
         total_bases = len(sequence)
